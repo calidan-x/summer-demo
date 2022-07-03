f0db2981db97e1f700d9cdea556dbdeca2a91ca8
Calidan <calidan@vip.qq.com>
2022-07-03 17:46:37 +0800

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import AWSXRay from 'aws-xray-sdk'
 import { summerStart, handler } from '@summer-js/summer'
-import './auto-imports'
 export { handler }
 
 summerStart({
   async before(config) {
     if (['dev', 'prod'].includes(process.env.SUMMER_ENV)) {
-      AWSXRay.captureMySQL(require('mysql'))
       AWSXRay.captureHTTPsGlobal(require('http'))
       AWSXRay.captureHTTPsGlobal(require('https'))
     }
